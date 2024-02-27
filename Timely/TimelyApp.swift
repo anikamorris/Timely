e46f1b951a56bcbd6561d6ed08be393e8b28f436
Anika Morris <amorris@octavebio.com>
2024-02-26 18:26:26 -0600

    Initial Commit

---
+++ Timely/TimelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelyApp.swift
+//  Timely
+//
+//  Created by Anika Morris on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TimelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
